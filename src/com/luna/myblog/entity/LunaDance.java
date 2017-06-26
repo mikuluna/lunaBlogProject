d6b87b87dedd45a8a45f2614c02428413d8f5f0f
mikuluna <449310419@qq.com>
2017-06-26 22:55:52 +0800

    dance entity
    upload dance entity

---
+++ src/com/luna/myblog/entity/LunaDance.java
@@ -0,0 +1,40 @@
+package com.luna.myblog.entity;
+
+public class LunaDance {
+	private int id;
+	private String imgFace;
+	private String danceUrl;
+	private String danceTitle;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getImgFace() {
+		return imgFace;
+	}
+
+	public void setImgFace(String imgFace) {
+		this.imgFace = imgFace;
+	}
+
+	public String getDanceUrl() {
+		return danceUrl;
+	}
+
+	public void setDanceUrl(String danceUrl) {
+		this.danceUrl = danceUrl;
+	}
+
+	public String getDanceTitle() {
+		return danceTitle;
+	}
+
+	public void setDanceTitle(String danceTitle) {
+		this.danceTitle = danceTitle;
+	}
+}
